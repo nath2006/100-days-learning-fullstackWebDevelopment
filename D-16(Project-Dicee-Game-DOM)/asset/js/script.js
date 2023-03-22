bcb753a5faaa7c3a279c843b0723df65cd314443
Nath <ananthapindika739@gmail.com>
2023-03-22 23:52:26 +0700

    100 Days Learn Fullstack Web Development

---
+++ D-16(Project-Dicee-Game-DOM)/asset/js/script.js
@@ -0,0 +1,26 @@
+//player 1
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+const randomDiceeImg = "dice" + randomNumber1 + ".png";
+
+const randomImageSource = "./asset/images/" + randomDiceeImg;
+
+const image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//player 2
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+const randomImageSource2 = "./asset/images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = " Play 1 wins";
+}else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = " Play 2 wins";
+}else {
+    document.querySelector("h1").innerHTML = " DRAW";
+}
