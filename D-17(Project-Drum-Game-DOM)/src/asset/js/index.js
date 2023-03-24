e1f6b914079725c3a2d4d9d7e50eb8e69533326b
Nath <ananthapindika739@gmail.com>
2023-03-24 22:57:07 +0700

    100 Days Learn Fullstack Web Dev

--- D-17(Project-Drum-Game-DOM)/src/asset/js/index.js
+++ D-17(Project-Drum-Game-DOM)/src/asset/js/index.js
@@ -1,6 +1,6 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
+for (const i = 0; i < numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
