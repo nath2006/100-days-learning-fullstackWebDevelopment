19f53905ac8f5e0af8b01992ecd6a3e227b6a8c9
Nath <ananthapindika739@gmail.com>
2023-04-11 15:50:44 +0700

    100 Days Learn Fullstack Developer

--- D-26(Project-Newsletter-Signup)/server.js
+++ D-26(Project-Newsletter-Signup)/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const request = require("request");
 const bodyParser = require("body-parser");
 
 const port = 9000;
@@ -7,6 +8,21 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+});
+
+app.post("/", (req, res) => {
+    let firstname = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+
+    console.log(`Hellow ${firstname} dengan nama belakang ${lastName} dan email ${email}`);
+});
+
 app.listen (port, () => {
     console.log(`localhost:${port}`);
 });
+
+//api
+//d6f995bfdfef44c5fffd85e6d8b0ca29-us9
