b30d37d1368d64f571b469001171df1ed120144b
Nath <ananthapindika739@gmail.com>
2023-05-05 23:55:52 +0700

    100 Days Learn Fullstack Web Dev

--- D-29(Todo-List-App-Using-EJS)/src/index.js
+++ D-29(Todo-List-App-Using-EJS)/src/index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+
 const port = 5000;
 
 app.get("/",(req, res) => {
-    res.send("Hello")
+    
+    let today = new Date();
+    let currentDay = today.getDay();
+    let day = "";
+
+    if (currentDay === 6 || currentDay === 0) {
+        day ="Nyantai";
+    }else {
+        day = "Kerja";
+    }
+
+    res.render("list", {currentDay : day} );
+    
 });
 
 app.listen(port, () => {
-    console.log`localhost:${port}`
+    console.log(`localhost:${port}`);
 });
