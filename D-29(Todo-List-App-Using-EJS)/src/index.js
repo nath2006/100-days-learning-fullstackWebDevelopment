fdb555d600483a544dc494ea54d5a4e7192e51af
Nath <ananthapindika739@gmail.com>
2023-05-11 00:19:37 +0700

    100 Days Learn Fullstack Web Dev

--- D-29(Todo-List-App-Using-EJS)/src/index.js
+++ D-29(Todo-List-App-Using-EJS)/src/index.js
@@ -3,47 +3,42 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views')); 
 
 const port = 5000;
+let items = [""];
 
 app.get("/",(req, res) => {
     
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
+    const options = {
+        weekday : 'long',
+        day : 'numeric',
+        month : 'long'
+    };
+    
+    const day= today.toLocaleDateString("en-US", options)
 
-    switch(currentDay) {
-        case 0 :
-            day = "Sunday"
-            break;
-        case 1 : 
-            day = "Monday"    
-            break;
-        case 2 : 
-            day = "Tuesday"
-            break;
-        case 3 : 
-            day = "Wednesdaya"
-            break;
-        case 4 : 
-            day = "Thrusday"
-            break;
-        case 5 : 
-            day = "Friday"
-            break;
-        case 6 : 
-            day = "Saturday"
-            break;
-        default :
-            day = " "
-    }
     
     res.render("list.ejs", 
-        {currentDay : day} 
+        {
+            currentDay : day,
+            newListItem : items
+        } 
     );
 });
+
+app.post("/", (req,res) => {
+    let item = req.body.newItems;
+    
+    items.push(item);
+
+    res.redirect('/');
+})
 
 app.listen(port, () => {
     console.log(`localhost:${port}`);
