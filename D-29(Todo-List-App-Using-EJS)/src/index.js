762bdc9936cf42cdc9094ddf99d403ca7e712724
Nath <ananthapindika739@gmail.com>
2023-05-16 00:34:53 +0700

    100 Days Learn Fullstack Web Dev

--- D-29(Todo-List-App-Using-EJS)/src/index.js
+++ D-29(Todo-List-App-Using-EJS)/src/index.js
@@ -10,8 +10,8 @@ app.set('view engine','ejs');
 app.set('views', path.join(__dirname, '/views')); 
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static)
-
+// app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(__dirname + '/public'));
 let tasks = [];
 const port = 5000;
 
