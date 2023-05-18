8d53b38d9f2da65523505279e054d2f34d9dfb65
Nath <ananthapindika739@gmail.com>
2023-05-18 23:42:48 +0700

    100 Days Learn Fullstack Web Dev

--- D-29(Todo-List-App-Using-EJS)/src/index.js
+++ D-29(Todo-List-App-Using-EJS)/src/index.js
@@ -1,18 +1,21 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
+
 const express = require('express');
 const bodyParser = require('body-parser');
-// eslint-disable-next-line no-unused-vars
 const path = require('path');
-
 const app = express();
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, '/views')); 
 
 app.use(bodyParser.urlencoded({extended:true}));
-// app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(__dirname + '/public'));
+
 let tasks = [];
+let workTask = [];
+
 const port = 5000;
 
 app.get('/',(req,res) => {
@@ -24,15 +27,27 @@ app.get('/',(req,res) => {
 		month:'long'
 	};
 	const day = today.toLocaleDateString('en-US',options);
-	res.render('list.ejs',{kindOfDay:day,tasks:tasks});
+
+	res.render('list.ejs',{ listTitle:day, newListItems:tasks });
 });
 
+app.get('/work', (req, res) => {
+	const title = 'Work List';
+	res.render('list.ejs',{ listTitle:title, newListItems:workTask });
+});
 
 app.post('/',(req,res) => {
-	const task = req.body.newItem;
-	tasks.push(task);
-	res.redirect('/');
+	const item = req.body.newItem;
+
+	if(req.body.list === 'Work' ) {
+		workTask.push(item);
+		res.redirect('/work');
+	}else {
+		tasks.push(item);
+		res.redirect('/');
+	}
 });
+
 
 app.listen(port,() => {
 	console.log(`Server running on ${port}`);
